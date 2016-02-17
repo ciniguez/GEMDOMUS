58a5a3a6ec4abef21897d63b5576cfcef49e7f30
ciniguez <carlos.iniguez79@gmail.com>
2016-02-17 20:45:12 +0100

    Incorporación de Pantalla AddGráfico. y efecto para aparecer gráfico.

--- pagesv2/controllerConsultas.js
+++ pagesv2/controllerConsultas.js
@@ -3,33 +3,16 @@
  */
 function GenDomusConsultas() {
 	var that = this;
-	that.numeroContenedoresGraficos = 6;
+	that.numeroContenedoresGraficosAllowed = 6;
+	that.idContenedorActual = 0;
+	that.prefijoContenedor = "grap";
+	that.altoGrafico = 290;
+	that.anchoGrafico = 290;
 	/**
 	 * Métodos de la Clase
 	 */
 	that.init = function() {
-		generarContenedoresGraficos(this.numeroContenedoresGraficos);
-/*
-		// Load the Visualization API and the corechart package.
-		google.charts.load('current', {
-			'packages' : ['corechart']
-		});
-
-		var options = {
-			'title' : 'Titulo Graph',
-			'width' : 300,
-			'height' : 300
-		};
-		// Create the data table.
-		var data = new google.visualization.DataTable();
-		data.addColumn('string', 'Topping');
-		data.addColumn('number', 'Slices');
-		data.addRows([['Mushrooms', 3], ['Onions', 1], ['Olives', 1], ['Zucchini', 1], ['Pepperoni', 2]]);
-		// Set a callback to run when the Google Visualization API is loaded.
-		google.charts.setOnLoadCallback(that.addGraph(data, options, 'contenedorGrafico0'));
-*/
 		$('#btnFilters').click(function() {
-			console.log("carlos");
 			if ($('.sidebar-offcanvas').css('background-color') == 'rgb(255, 255, 255)') {
 				$('.filters').attr('tabindex', '-1');
 			} else {
@@ -38,34 +21,83 @@ function GenDomusConsultas() {
 			$('.row-offcanvas').toggleClass('active');
 
 		});
-		$("#sortable1").sortable().disableSelection();
+		$('#btnAddGraph').click(function() {
+			$("#frameNewGraph").modal();
+		});
+		$('#btnDeleteGraph').click(function() {
+			//TO DO
+		});
+		$('#frameNewGraph .btn-success').click(function() {
 
-		//Agregar Graficos
-		/*
-		for (var i = 0; i < that.numeroContenedoresGraficos; i++) {
-			that.addGraph();
-		}
-		*/
+			//Obtengo las variables del gráfico
+			var titulo = $('#frameNewGraph #txtTitle').val();
+			console.log(titulo);
+			var tipoGrafico = 0;
+			console.log(tipoGrafico);
+			var variablePrincipal = $('#frameNewGraph select[id="lstMainVariable"]').val();
+			console.log(variablePrincipal);
+			var variableSecundaria = $('#frameNewGraph select[id="lstSecondVariable"]').val();
+			console.log(variableSecundaria);
+			var operacion = $('#frameNewGraph select[id="lstAgregation"]').val();
+			console.log(operacion);
+			
+
+			//Add the graph to cotainer created.
+			var options = {
+				'title' : titulo,
+				'width' : that.anchoGrafico,
+				'height' : that.altoGrafico
+			};
+			that.addGraph(options);
+
+		});
+		$("#sortable1").sortable().disableSelection();
 
 	};
 	/**
-	 * Draw a statisctic graph
+	 * Create a graph container inside UL "sortable" and draw a statisctic graph inside it.
 	 * @param {Object} data Data JSON to fill the graph
 	 * @param {Object} options A object in the way {'title':'xx', 'width':00 , 'height':00}
 	 * @param {Object} idContenedor Cotent Identificator
 	 */
-	that.addGraph = function(data, options, idContenedor) {
+	that.addGraph = function(options) {
+		//Create a new container in the top of List of Graphs.
+		if (that.idContenedorActual === null) {
+			that.idContenedorActual = 0;
+		}
+		if (that.idContenedorActual <= that.numeroContenedoresGraficosAllowed) {
+			var idName = that.prefijoContenedor + that.idContenedorActual;
+			var liElement = '<div id="' + idName + '" class="contenedorGrafico"></div>';
 
-		// Instantiate and draw our chart, passing in some options.
-		var chart = new google.visualization.PieChart(document.getElementById(idContenedor));
-		chart.draw(data, options);
-	};
-	function generarContenedoresGraficos(numeroCuadros) {
-		for (var i = 0; i < numeroCuadros; i++) {
-			$("#dashboard ul").append('<div id="contenedorGrafico' + i + '" class="contenedorGrafico">AQUI EL GRAFICO ' + i + '</div>');
+			$(liElement).prependTo('#dashboard ul');
+			//Efecto feedback para el usuario (cambio de color para alertar el nuevo gráfico)
+
+			$('#' + idName).animate({
+				'border-width' : "120px",
+			}, 'slow');
+			//restauro estado inicial
+			$('#' + idName).animate({
+				'border-width' : "1px",
+			}, 'fast', function() {
+				// Create the data table.
+				var data = new google.visualization.DataTable();
+				data.addColumn('string', 'Topping');
+				data.addColumn('number', 'Slices');
+				data.addRows([['Mushrooms', 3], ['Onions', 1], ['Olives', 1], ['Zucchini', 1], ['Pepperoni', 2]]);
+				// Set a callback to run when the Google Visualization API is loaded.
+				// Instantiate and draw our chart, passing in some options.
+				var chart = new google.visualization.PieChart(document.getElementById(idName));
+				chart.draw(data, options);
+				//Actualizo el contador de Contenedores
+				that.idContenedorActual += 1;
+
+			});
+
+		} else {
+			console.log("Ha llegado al límite de los " + that.numeroContenedoresGraficosAllowed + " graficos");
 		}
 
-	}
+	};
 
 }
 
